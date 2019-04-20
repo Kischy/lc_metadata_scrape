370e5c27b66a02bd95fa086178bb9c561c9c36b8
Christian Häge <christian.haege@gmx.net>
2019-04-20 23:19:54 +0200

    Added main file

---
+++ copy_data_from_web_of_science.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 16:53:01 2019
+
+@author: Kischy
+"""
+
+import sys
+import requests
+import time
+from bs4 import BeautifulSoup
+
+import re
+import mechanize
+
+
+url = "https://apps.webofknowledge.com/"
+search_string = "\"liquid crystal\""
+
+
+
+br = mechanize.Browser()
+
+
+
+
+
+time.sleep(1)
+
+
+
